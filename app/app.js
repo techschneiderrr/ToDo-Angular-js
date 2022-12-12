07ffa4d8dbe3c45066db9f9c2c55f5eb14b265e3
Manav Deep Singh Lamba <manavdeep.singhlamba@gmail.com>
2022-12-12 11:06:45 +0530

    commit

--- app/app.js
+++ app/app.js
@@ -1,44 +1,65 @@
 var variable = angular.module('module',[]);
 
 variable.controller('myController', ['$scope', function($scope){
-    $scope.meas="hello this is a variable....";
-    $scope.list=[
+    
+    
+    $scope.removeBoy = function(boy){
+      var removedBoy = $scope.boys.indexOf(boy);
+      $scope.boys.splice(removedBoy, 1);
+    }
+
+    $scope.addBoy = function(){
+      const belt = $scope.newBoy.belt.charAt(0).toUpperCase() + $scope.newBoy.belt.slice(1);
+      const name = $scope.newBoy.name.charAt(0).toUpperCase() + $scope.newBoy.name.slice(1);
+      $scope.boys.push({
+        name:name,
+        belt:belt,
+        rate:parseInt($scope.newBoy.rate),
+        available:true
+      });
+        console.log($scope.boys);
+      $scope.newBoy.name="";
+      $scope.newBoy.belt="";
+      $scope.newBoy.rate="";
+    }
+
+    $scope.boys=[
 
         {
-            "uuid": "dade69b4-4f5a-8528-247b-219e5a1facd6",
-            "displayName": "Fade",
-            "rate":345,
-            "description": "Turkish bounty hunter Fade unleashes the power of raw nightmare to seize enemy secrets. Attuned with terror itself, she hunts down targets and reveals their deepest fears - before crushing them in the dark."
+            name: "Fade",
+            belt:"Blue",
+            rate:345,
+            available:true
           },
           {
-            "uuid": "5f8d3a7f-467b-97f3-062c-13acf203c006",
-            "displayName": "Breach",
-            "rate":376,
-            "description": "The bionic Swede Breach fires powerful, targeted kinetic blasts to aggressively clear a path through enemy ground. The damage and disruption he inflicts ensures no fight is ever fair."
+            name: "Breach",
+            belt:"Red",
+            rate:376,
+            available:true
           },
           {
-            "uuid": "f94c3b30-42be-e959-889c-5aa313dba261",
-            "displayName": "Raze",
-            "rate":987,
-            "description": "Raze explodes out of Brazil with her big personality and big guns. With her blunt-force-trauma playstyle, she excels at flushing entrenched enemies and clearing tight spaces with a generous dose of \"boom\"."
+            name: "Raze",
+            belt:"Green",
+            rate:987,
+            available:true
           },
           {
-            "uuid": "22697a3d-45bf-8dd7-4fec-84a9e28c69d7",
-            "displayName": "Chamber",
-            "rate":320,
-            "description": "Well dressed and well armed, French weapons designer Chamber expels aggressors with deadly precision. He leverages his custom arsenal to hold the line and pick off enemies from afar, with a contingency built for every plan."
+            name: "Chamber",
+            belt:"Purple",
+            rate:320,
+            available:true
           },
           {
-            "uuid": "601dbbe7-43ce-be57-2a40-4abd24953621",
-            "displayName": "KAY/O",
-            "rate":987,
-            "description": "KAY/O is a machine of war built for a single purpose: neutralizing radiants. His power to suppress enemy abilities cripples his opponents' capacity to fight back, securing him and his allies the ultimate edge."
+            name: "KAY/O",
+            belt:"Orange",
+            rate:987,
+            available:true
           },
           {
-            "uuid": "6f2a04ca-43e0-be17-7f36-b3908627744d",
-            "displayName": "Skye",
-            "rate":786,
-            "description": "Hailing from Australia, Skye and her band of beasts trailblaze the way through hostile territory. With her creations hampering the enemy, and her power to heal others, the team is strongest and safest by Skye's side.",
+            name: "Skye",
+            belt:"Black",
+            rate:786,
+            available:false
           }
 
     ];
